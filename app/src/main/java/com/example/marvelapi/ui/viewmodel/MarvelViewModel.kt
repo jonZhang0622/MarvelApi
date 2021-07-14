098007e03a12805e1fb2b325f6ef29181a636e0f
John Zang <jnzhang622@gmail.com>
2021-07-13 21:45:53 -0400

    Created MarvelViewModel.kt

---
+++ app/src/main/java/com/example/marvelapi/ui/viewmodel/MarvelViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.marvelapi.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.marvelapi.data.model.MarvelResponse
+import com.example.marvelapi.data.repos.MarvelRepo
+import com.example.marvelapi.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MarvelViewModel @Inject constructor(private val marvelRepo: MarvelRepo) : ViewModel() {
+
+    private var _marvelResource = MutableLiveData<Resource<MarvelResponse>>()
+    val marvelResource get() = _marvelResource
+
+    init {
+        getComics()
+    }
+
+    private fun getComics() {
+        _marvelResource.value = Resource.Loading
+        viewModelScope.launch(Dispatchers.IO) {
+            val marvelResource = marvelRepo.getComics()
+            _marvelResource.postValue(marvelResource)
+        }
+    }
+}
